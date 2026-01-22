be13527347ac3c223c56483cef2a9ffa258871f5
chris3es <chrisdebiase2@gmail.com>
2026-01-21 21:51:50 -0500

    Create PulseCoreClient.java

---
+++ PulseCoreClient.java
@@ -0,0 +1,10 @@
+package com.chris.pulsecore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PulseCoreClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-only stuff here
+    }
+}
